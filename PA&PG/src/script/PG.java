d3984718814a04f5cd302443e9dbbedc993a4c27
thomas_manzi <54033366+thomas-manzi@users.noreply.github.com>
2020-03-03 18:14:24 -0300

    começo da v2

---
+++ PA&PG/src/script/PG.java
@@ -0,0 +1,11 @@
+package script;
+
+public class PG {
+	private int razao;
+	private int progressao;
+	
+	PG(int razao, int progressao){
+		this.progressao=progressao;
+		this.razao=razao;
+	}
+}
---
+++ PA&PG/src/script/main.java
@@ -0,0 +1,14 @@
+package script;
+
+public class main {
+
+	public static void main(String[] args) {
+		PG pg = new PG(2, 4);
+		PA pa= new PA(3,10,1);
+		System.out.println(pa.resultadoPA());
+		
+
+		
+	}
+
+}
